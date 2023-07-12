1943a4eed24df8db71293c513cbac2f15e0d43fc
Callum Alexander <callumalexander.personal@gmail.com>
2023-07-12 16:28:23 +0100

    Got a toggle light and darkmode working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from "./Dashboard";
+import {useState} from "react";
 
 function App() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleToggleMode = () => {
+    setIsDarkMode(!isDarkMode);
+    updateColorScheme();
+  };
+
+  const updateColorScheme = () => {
+    const root = document.documentElement;
+    if (isDarkMode) {
+      root.style.setProperty('--color-primary', '#61afef');
+      root.style.setProperty('--color-secondary', '#98c379');
+      root.style.setProperty('--color-background', '#282c34');
+      root.style.setProperty('--color-text', '#abb2bf');
+    } else {
+      root.style.setProperty('--color-primary', '#add8e6');
+      root.style.setProperty('--color-secondary', '#90ee90');
+      root.style.setProperty('--color-background', '#f5f5f5');
+      root.style.setProperty('--color-text', '#333333');
+    }
+  };
+
   return (
-    <div className="App">
+    <div className={`App ${isDarkMode ? 'dark-mode' : ''}`}>
+        <button className="toggle-button" onClick={handleToggleMode}>
+        Toggle Mode
+      </button>
       <div className="Dashboard-dv">
         <Dashboard />
       </div>
