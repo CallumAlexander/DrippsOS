82aa996b9218c21b44be1603e378ac78145831f3
Callum Alexander <callumalexander.personal@gmail.com>
2023-08-07 10:32:48 +0100

    Added chart titles to all 6, just the water wheel for now.

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -12,19 +12,19 @@ function Dashboard({data}) {
                         <TimeSeriesChart data={data} title_={"Energy generated"} subtitle_={"In Kwh"}/>
                     </div>
                     <div className="chart">
-                        <TimeSeriesChart data={data}/>
+                        <TimeSeriesChart data={data} title_={"Main Sluice Height"} subtitle_={"Metres"}/>
                   </div>
                   <div className="chart">
-                      <TimeSeriesChart data={data}/>
+                      <TimeSeriesChart data={data} title_={"Wheel Sluice Height"} subtitle_={"Metres"}/>
                   </div>
                   <div className="chart">
-                      <TimeSeriesChart data={data}/>
+                      <TimeSeriesChart data={data} title_={"Water throughput"} subtitle_={"Gallons per second in the trough"}/>
                   </div>
                   <div className="chart">
-                      <TimeSeriesChart data={data}/>
+                      <TimeSeriesChart data={data} title_={"Wheel RPM"}/>
                   </div>
                   <div className="chart">
-                      <TimeSeriesChart data={data}/>
+                      <TimeSeriesChart data={data} title_={"Valve height"} subtitle_={"Metres"}/>
                   </div>
                 </div>
             </div>
